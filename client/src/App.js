483ff5fe054a2d17346c8808eebb7c9a86c0d43a
tykowale <tykowale@gmail.com>
2018-11-16 10:29:09 -0600

    bring in router

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
 import "./App.css";
 import NavBar from "./components/layout/NavBar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavBar/>
-        <Landing/>
-        <Footer/>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+
+          <NavBar/>
+
+          <Route exact path="/" component={Landing}/>
+
+          <div className="container">
+            <Route exact path="/register" component={Register}/>
+            <Route exact path="/login" component={Login}/>
+          </div>
+
+          <Footer/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -18,10 +18,10 @@ const User = require("../../models/User");
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 
-// @route   POST api/users/register
+// @route   POST api/users/auth
 // @desc    Register user
 // @access  Public
-router.post("/register", (req, res) => {
+router.post("/auth", (req, res) => {
   const { errors, isValid } = validateRegisterInput(req.body);
 
   // Check Validation
