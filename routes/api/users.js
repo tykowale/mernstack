e2380dce662e96f5e5a64072855490249d18b8e8
tykowale <tykowale@gmail.com>
2018-11-16 11:07:30 -0600

    working register page

--- routes/api/users.js
+++ routes/api/users.js
@@ -18,10 +18,10 @@ const User = require("../../models/User");
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 
-// @route   POST api/users/auth
+// @route   POST api/users/register
 // @desc    Register user
 // @access  Public
-router.post("/auth", (req, res) => {
+router.post("/register", (req, res) => {
   const { errors, isValid } = validateRegisterInput(req.body);
 
   // Check Validation
