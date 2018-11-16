5e975e19b5ac88df2855ab8baf5320c8b4d134d1
tykowale <tykowale@gmail.com>
2018-11-15 18:02:45 -0600

    connect to mongodb

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
 
 const app = express();
+
+// Connect to MongoDB
+mongoose.connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello!"));
 
