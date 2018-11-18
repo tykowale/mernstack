13a89947992d4c63c8380ef7b19b0fdd9b06ff32
tykowale <tykowale@gmail.com>
2018-11-17 23:03:00 -0600

    user dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 import { getCurrentProfile } from "../../actions/profileActions";
-
-const mapState = (state) => ({});
-
-const actions = {
-  getCurrentProfile
-};
+import Spinner from "../common/Spinner";
 
 class Dashboard extends Component {
   componentDidMount() {
@@ -15,13 +12,56 @@ class Dashboard extends Component {
   }
 
   render() {
+    const { user } = this.props.auth;
+    const { profile, loading } = this.props.profile;
+
+    let dashboardContent;
+
+    if (profile === null || loading) {
+      dashboardContent = <Spinner/>;
+    } else {
+      if (Object.keys(profile).length > 0) {
+        dashboardContent = <h4> TODO: DISPLAY PROFILE </h4>;
+      } else {
+        dashboardContent = (
+          <div>
+            <p className="load text-muted">Welcome {user.name}</p>
+            <p>You have not yet set up a profile, please add some info</p>
+            <Link to="/create-profile" className="btn btn-lg btn-info">Create Profile</Link>
+          </div>
+        );
+      }
+    }
+
     return (
-      <div>
-        <h1> Dashboard </h1>
+      <div className="dashboard">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4"> Dashboard </h1>
+              {dashboardContent}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+const mapState = (state) => ({
+  profile: state.profile,
+  auth: state.auth
+});
+
+const actions = {
+  getCurrentProfile
+};
+
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+};
 
 export default connect(mapState, actions)(Dashboard);
 
