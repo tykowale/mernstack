3994f8f24a49ce6fa69ba41260239fdbaf417b1c
tykowale <tykowale@gmail.com>
2018-11-18 09:36:07 -0600

    section 9, 4 of 8. Looks like repeating stuff now

---
+++ client/src/components/createProfile/CreateProfile.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import {connect} from "react-redux";
+import PropTypes from "prop-types";
+
+class CreateProfile extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+CreateProfile.propTypes =  {
+
+};
+
+const mapState = (state) => ({
+
+});
+
+export default connect(mapState)(CreateProfile);
